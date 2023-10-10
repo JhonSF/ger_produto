2389e41a69d57fa934a0f37a964236257622fa24
“Jonathas” <jhonsf.pessoal@gmail.com>
2023-10-09 21:18:03 -0300

    inciando o projeto

---
+++ ger_produto/src/test/java/br/com/stdev/ger_produto/GerProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stdev.ger_produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
